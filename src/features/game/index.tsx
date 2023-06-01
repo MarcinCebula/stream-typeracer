4fb0af3d4b7821497643f81d1c7c93a46977777b
Marcin K Cebula <Marcin.K.Cebula@gmail.com>
2023-06-01 22:05:33 +0800

    Add my own layout and add src/features

---
+++ src/features/game/index.tsx
@@ -0,0 +1,9 @@
+export const GameHome = () => {
+  return (
+    <div className="flex flex-col items-center justify-start min-h-screen py-6">
+      <h1 className="font-sansDigit text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+        Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+      </h1>
+    </div>
+  );
+};
