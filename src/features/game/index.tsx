5382316181077d607f645633db87caea374877da
Marcin K Cebula <Marcin.K.Cebula@gmail.com>
2023-06-01 22:52:59 +0800

    Added DaisyUI and chat bubbles

--- src/features/game/index.tsx
+++ src/features/game/index.tsx
@@ -26,8 +26,46 @@ export const ChatBoxInput = () => (
 );
 
 export const ChatBox = () => (
-  <div className="flex h-96 w-full flex-col rounded-lg border-4 border-violet-600 bg-violet-300 p-4">
-    test
+  <div className="flex h-96 w-full flex-col overflow-y-scroll rounded-lg border-4 border-violet-600 bg-violet-300 p-4">
+    <ChatBoxSampleChat />
+  </div>
+);
+
+export const ChatBoxSampleChat = () => (
+  <div className="flex flex-col">
+    <div className="chat chat-start">
+      <div className="chat-bubble chat-bubble-primary">
+        What kind of nonsense is this
+      </div>
+    </div>
+    <div className="chat chat-start">
+      <div className="chat-bubble chat-bubble-secondary">
+        Put me on the Council and not make me a Master!??
+      </div>
+    </div>
+    <div className="chat chat-start">
+      <div className="chat-bubble chat-bubble-accent">
+        {`That's never been done in the history of the Jedi. It's insulting!`}
+      </div>
+    </div>
+    <div className="chat chat-end">
+      <div className="chat-bubble chat-bubble-info">Calm down, Anakin.</div>
+    </div>
+    <div className="chat chat-end">
+      <div className="chat-bubble chat-bubble-success">
+        You have been given a great honor.
+      </div>
+    </div>
+    <div className="chat chat-end">
+      <div className="chat-bubble chat-bubble-warning">
+        To be on the Council at your age.
+      </div>
+    </div>
+    <div className="chat chat-end">
+      <div className="chat-bubble chat-bubble-error">
+        {`It's never happened before.`}
+      </div>
+    </div>
   </div>
 );
 
