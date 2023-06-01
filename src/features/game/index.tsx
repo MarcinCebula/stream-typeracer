014ac5dabe95e909f52c9ca54812a50154215b25
Marcin K Cebula <Marcin.K.Cebula@gmail.com>
2023-06-01 22:24:49 +0800

    update text

--- src/features/game/index.tsx
+++ src/features/game/index.tsx
@@ -1,9 +1,21 @@
 export const GameHome = () => {
   return (
-    <div className="flex flex-col items-center justify-start min-h-screen py-6">
-      <h1 className="font-sansDigit text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-        Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-      </h1>
+    <div className="flex min-h-screen flex-col items-center justify-start py-7">
+      <div className="flex flex-col space-y-3 items-center text-center">
+        <h1 className="font-sansDigit text-5xl font-bold tracking-tight text-white">
+          Multiplayer{" "}
+          <span className="text-[hsl(280,100%,70%)]">TypeRacer</span>
+        </h1>
+        <h2 className="font-sansDigit text-2xl font-extralight tracking-tight text-white">
+          with{" "}
+          <a
+            href="https://replicache.dev/"
+            className="text-[hsl(280,100%,70%)] underline font-semibold"
+          >
+            Replicache
+          </a>
+        </h2>
+      </div>
     </div>
   );
 };
